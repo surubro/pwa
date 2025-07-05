b77924ee6b5a7825a25a8920d131d77be0834439
surubro <chicken3301@gmail.com>
2025-07-06 02:15:03 +0530

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+
+const CACHE_NAME = "offline-menu-v1";
+const OFFLINE_URLS = [
+  "index.html",
+  "manifest.json",
+  "service-worker.js",
+  "icons/icon-192.png",
+  "icons/icon-512.png"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(OFFLINE_URLS);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
